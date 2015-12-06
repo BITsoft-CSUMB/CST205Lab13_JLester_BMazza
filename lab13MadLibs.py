0197a9d9b45cbfe9c94fedb26fd4dd35d48180fe
Brittany Mazza <bmazza555@gmail.com>
2015-12-05 16:31:44 -0800

    Add mad libes outline

--- lab13MadLibs.py
+++ lab13MadLibs.py
@@ -3,4 +3,35 @@ John Lester & Brittany Mazza
 CST 205 Multimedia Design and Programming
 
 Lab #13 Pt. 1: Mad Libs
+
+Mad Lib Outline:
+
+1. name person
+2. city name
+3. month of the year
+4. adjective
+5. verb past tense
+6. adjective
+7. adjective
+8. noun
+9. verb ending in “ing”
+10. noun
+11. noun place
+12. number
+13. your name
+14. adjective
+15. adjective
+16. verb ending in “ing”
+17. holiday
+
+“___(1)’s Strange Afterlife”
+People who were out and about at various ___(2) night spots on a night in
+___(3) happened upon something ___(4). They ___(5) a throng of men and
+women, wearing ___(6) wigs and identical ___(7) shirts, flood the
+neighborhood’s ___(8), ___(9) a ___(10) or two before moving on.
+
+This was the third annual ___(1) Bar Crawl, which is exactly what it sounds
+like: more than 50 people dressed as their hero, the public-television icon
+___(1), paying tribute in ___(11) after ___(11), ___(12) years after
+their death.
 """
